cd46335199c9b240bd35f76e9d1a826547ae5a13
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-17 09:33:01 +0000

    datatypes and variable

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert("hello");// we are using node js not browser
+
+console.log(3+3)
+
+console.log("anubhav")
+let name  = "anubhav"
+let age = 18
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint 
+// string=> ""
+// boolean=> true/false
+// null => standalone value (object)
+// undefined => value is not defined (undefined)
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
--- basics/variable.js
+++ basics/variable.js
@@ -2,8 +2,11 @@ const accountId = 14553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-
+let accountState
 //accountId = 2
 
 console.table([accountEmail , accountPassword , accountCity , accountId])
-
+/*prefer not to use var because of issue 
+in block scope and 
+functional scope*/
+console.log(accountState)
