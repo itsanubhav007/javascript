c71dc10108ad72114f8e2ad2a1052ae27711b897
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-17 10:07:06 +0000

    conversionOperations

---
+++ basics/conversionOperations.js
@@ -0,0 +1,27 @@
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//console.log(stringNumber)
+
+
+/*let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+console.log(2**3);
+
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" + 2 + 2);
+console.log(1 +2 +"2");
+console.log((3+4)*5 %3);*/
+console.log(+true);
+console.log(+""); //==> typeconversion to integer
+
+let num1  , num2 , num3
+
+num1 = num2 = num3 = 2+2
+console.log(num1)
+
+let gameCounter =  100;
+gameCounter++;
+console.log(gameCounter);
