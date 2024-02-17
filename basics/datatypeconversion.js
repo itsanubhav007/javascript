9e8f59a1cd06f16fc8d18be7e57fb08ea71de9ca
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-17 09:47:54 +0000

    type conversion

---
+++ basics/datatypeconversion.js
@@ -0,0 +1,30 @@
+let score = "kfjio";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false=>0;
+//undefined => NaN
+
+let isLoggedIn = "anubhav"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+
+// 1 => true ; 0 => false
+// ""=>true ;  "wdpifjwoi"=> false
+
+let someNumber = 33
+let stringNumber  = String(33);
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
