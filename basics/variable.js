fd0ac10c2309351d6619fbb4fdf2e31c4d59ee0a
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-17 09:10:04 +0000

    second commit

---
+++ basics/variable.js
@@ -0,0 +1,9 @@
+const accountId = 14553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+//accountId = 2
+
+console.table([accountEmail , accountPassword , accountCity , accountId])
+
