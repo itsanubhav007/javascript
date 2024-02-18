a7b5a77bc794f2e09bd8ea034173ef5b838aaf41
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-18 12:36:45 +0000

    object Api

--- basics/object.js
+++ basics/object.js
@@ -97,3 +97,4 @@ console.log(Object.values(tinderUser));
 console.log(Object.entries(tinderUser));
 
 console.log(tinderUser.hasOwnProperty('isLogged'));
+
---
+++ basics/objectapi.js
@@ -0,0 +1,23 @@
+const course = {
+
+    coursename : "js in hindi",
+    price : "999",
+    courseInstructor : "anubhav"
+}
+
+//const {courseInstructor} =course
+//console.log(courseInstructor);
+
+const {courseInstructor : instructor} =course
+
+console.log(instructor);
+//
+//{
+//    name : "anubhav" , 
+//    coursename : "js in hindi",
+//    price: "free"
+//}
+//
+
+
+
