de715a9a576bc40776def24155f3da4ea8ff7458
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-02-18 10:24:57 +0000

    array

--- basics/arrays.js
+++ basics/arrays.js
@@ -42,6 +42,26 @@ const myn2 = myArr.splice(1,3)
 console.log(myn2);
 console.log("C " ,myArr);
 
+const marvelHeroes = ["thor" , "spiderMan" , "IronMan"]
+const dc = ["superman" , "flash" , "batman"]
 
+//marvelHeroes.push(dc);
+//console.log(marvelHeroes);
+//const allHeroes = marvelHeroes.concat(dc)
+//console.log(marvelHeroes)
+//console.log(allHeroes);
 
+//const all_new_heroes = [...marvelHeroes,...
+//dc]
+//console.log(all_new_heroes);
+//const another_Array = [1 ,2 ,3 , [4 , ,5 ,6] ,7 , [6 , 7 ,[4 ,5]]];
+//const real_another_array = another_Array.flat(Infinity)
+//console.log(real_another_array);
+//console.log(Array.isArray("Hitesh"));
+console.log(Array.from("Anubhav"));
+console.log(Array.from({name : "anubhav"})); // **interesting**
 
+let score1 =100
+let score2  = 200
+let score3 = 300
+console.log(Array.of(score1 ,score2 ,score3));
