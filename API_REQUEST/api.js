a4db76903ac2682953188822d977663a1915dd5e
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-03-04 13:59:37 +0000

    promises

---
+++ API_REQUEST/api.js
@@ -0,0 +1,3 @@
+// talking language between two system.
+let a =prompt('enter the value');
+console.log(a);
---
+++ Promise/project.js
@@ -0,0 +1,7 @@
+// queue  me lag chuka hai par abhi pura nahi ho sakta bad me hoga.
+
+//fetch('').then().catch().finally(); 
+
+// Q and blue bird
+
+const promise1One = new Promise(); 
