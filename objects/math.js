5c3a43d7c31b009a79cc439b02744c578604fe65
itsanubhav007 <130907968+itsanubhav007@users.noreply.github.com>
2024-03-06 12:31:16 +0000

    Object

---
+++ objects/math.js
@@ -0,0 +1,26 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math , "PI");
+
+console.log(descriptor);
+
+
+//console.log(Math.PI);
+//Math.PI = 5;           
+//console.log(Math.PI);    
+
+const chai = {
+  name : 'ginger -chai',
+  price: 250,
+  isAvailable : true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+Object.defineProperty(chai , 'name' , {
+    writable : false , 
+    enumerable : false
+})
+console.log(Object.getOwnPropertyDescriptor(chai , "name"));
+
+
+//Object.entries for loop in Object
+
